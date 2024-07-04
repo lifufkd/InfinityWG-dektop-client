3658241a215ce84194cc252a04e279d1c22833e9
lilfufkd <dmsemen45@gmail.com>
2024-07-04 22:06:59 +0300

    Add file structure

---
+++ app.py
@@ -0,0 +1,13 @@
+##########################
+#       Created By       #
+#          SBR           #
+##########################
+import fastapi
+##########################
+config_path = 'config.json'
+##########################
+
+
+if __name__ == '__main__':
+    app = fastapi.FastAPI()
+    
