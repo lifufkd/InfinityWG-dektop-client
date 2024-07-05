a9478574d15366db6190a3fdcf16c828cedfb628
lilfuz <lilfuz@gmail.com>
2024-07-05 03:27:12 +0300

    Add sidebar

--- app.py
+++ app.py
@@ -2,12 +2,31 @@
 #       Created By       #
 #          SBR           #
 ##########################
-import fastapi
+import sys
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import Qt
+from qfluentwidgets import setTheme, Theme
+from PySide6.QtGui import QIcon
+from qframelesswindow import FramelessWindow
+from UI.sidebar import SideBar
+from UI.TitleBar import CustomTitleBar
 ##########################
 config_path = 'config.json'
 ##########################
 
 
+class Window(FramelessWindow):
+    def __init__(self):
+        super().__init__()
+        setTheme(Theme.AUTO)
+        SideBar(self)
+
+
+
+
 if __name__ == '__main__':
-    app = fastapi.FastAPI()
+    app = QApplication(sys.argv)
+    w = Window()
+    w.show()
+    app.exec()
     
